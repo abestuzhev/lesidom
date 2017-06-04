72603c1d15f0f2d077f13d5de142bb283d1afc0d
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2017-06-04 23:25:05 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -87,10 +87,10 @@ $("#top-product-slider, #new-product-slider, #discount-product-slider").owlCarou
 	}
 });
 
-$(".to-top").click(function () {
-	$("body,html").animate({scrollTop: 0}, 400);
-	return false;
-});
+// $(".to-top").click(function () {
+// 	$("body,html").animate({scrollTop: 0}, 400);
+// 	return false;
+// });
 
 
 //site-map page
@@ -363,6 +363,27 @@ $(".personal__tab-caption").click(function() {
         $('.filter').toggle();
         $('.c-popup_bg').removeClass('is-visible');
         $('body').removeClass('body-popup');
+    });
+
+    //кнопка вверх
+    var offset = 100,
+        offset_opacity = 1200,
+        scroll_top_duration = 700,
+        $back_to_top = $('.to-top');
+    //hide or show the "back to top" link
+    $(window).scroll(function(){
+        ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+        if( $(this).scrollTop() > offset_opacity ) {
+            $back_to_top.addClass('cd-fade-out');
+        }
+    });
+    //smooth scroll to top
+    $back_to_top.on('click', function(event){
+        event.preventDefault();
+        $('body,html').animate({
+                scrollTop: 0
+            }, scroll_top_duration
+        );
     });
 });
 
